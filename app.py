426f7628198eb006d5646b160fa2155c87ec61cd
Bautimoresco <bautimoresco03@gmail.com>
2024-06-07 19:00:50 -0300

    version final

--- app.py
+++ app.py
@@ -15,22 +15,39 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 st.markdown(
     """
     <style>
-    /* Set the background color and text color for the main container */
+  h1, h2, h3, h4, h5, h6 {
+    color: white; /* Cambia el color de los subtítulos */
+}
+
+
     [data-testid="stAppViewContainer"] {
-        background-color: #228B22;  /* Planta verde */
+        background-color: #6AB66A;  /* Planta verde */
     }
-    /* Set the background color and text color for the header */
+   
     [data-testid="stHeader"] {
-        background-color: #228B22;
-    }
-    /* Set the background color and text color for the sidebar */
-    [data-testid="stSidebar"] {
-        background-color: #228B22;
+        background-color:#6AB66A;
     }
 
     /* Set the text color for all text elements */
-    body, p, div, info {
+    body, p, div, info, {
         color: white}
+    
+        /* Custom style for the plantIA information box */
+    .info-box {
+        background-color: rgba(255, 255, 255, 0.8);  /* Transparent white background */
+        color: black;  /* Black text color */
+        padding: 15px;
+        border-radius: 10px;
+        margin-bottom: 10px;
+    }
+.stTextInput>div>label,
+.stTextArea>label,
+.stSelectbox>label {
+    color: white;
+}
+
+    
+
     </style>
     """,
     unsafe_allow_html=True
@@ -57,35 +74,58 @@ def classify_plant(img):
     return class_name, confidence_score
 
 # Function to generate advice based on the plant's condition
-def generate_advice(label):
-    client = OpenAI(api_key="YOUR_API_KEY_HERE")
-    response = client.completions.create(
-        model="gpt-3.5-turbo-instruct",
-        prompt=f"Sos un experto en jardinería y tenes que recomendar solo 3 consejos para cuidar una planta que está {label}.",
-        temperature=0.5,
-        max_tokens=300,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
-    )
-    return response.choices[0].text
 
 # Streamlit App layout
 # Agregar el logo al lado del título
 st.image("logo.png", width=850) 
+# Streamlit App layout
+
+
+col1, col2 = st.columns(2)
+with col1:
+    st.subheader("Acerca de la app")
+    st.markdown(
+        """
+        <div class="info-box">
+        plantIA es una aplicación diseñada para ayudarte a identificar el estado de salud de tus plantas y proporcionarte consejos útiles para su cuidado. Utilizando inteligencia artificial y aprendizaje automático, plantIA puede analizar imágenes de plantas y ofrecer diagnósticos precisos junto con recomendaciones de expertos.                                                                                                                                                                                                                                       
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
+    st.subheader("¿Cómo Funciona?")
+    st.markdown(
+        """
+    <div class="info-box">
+    
+    1. Cargá una foto de tu planta.
+    
+    2. La IA analiza la imagen.
+    
+    3. Vas a recibir un diagnostico del estado de tu planta.
+    </div>
+        """,
+        unsafe_allow_html=True
+    )
+
+with col2:
+    st.subheader("Beneficios")
+    st.markdown(
+        """
+        <div class="info-box">
+        
+        - Diagnósticos Precisos: La IA ofrece resultados precisos y confiables.
+        
+        - Fácil de Usar: Cargá una foto y obtené resultados en cuestión de segundos.
+       
+        - Consejos para vos: Recibí recomendaciones específicas para el cuidado de tus plantas.
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
 
 
 
-
-
-
-
-
-
-
-st.subheader("""Cargá una foto de una hoja de tu planta y determiná su estado.""")
-
-st.subheader("""También podés generar consejos de cuidado 🌿""")
+st.subheader("""Cargá la foto🌿""")
 
 input_img = st.file_uploader("Elegir imagen", type=['jpg', 'png', 'jpeg'])
 
@@ -103,7 +143,40 @@ if input_img is not None:
                 label_description = label.split(maxsplit=1)[1]  # Divide la etiqueta por el primer espacio y toma el segundo elemento
                 label2 = label_description  # Guarda la descripción en label2
                 st.success(label2)  # Muestra la etiqueta sin el número
-        with col3:
-            st.info("Consejos de cuidado")
-            result = generate_advice(label2)
-            st.success(result)
+
+
+
+
+
+
+def vista_consejos():
+    st.subheader("Consejos de Cuidado")
+    st.markdown(
+        """
+        <div class="info-box">
+
+Cuidar de las plantas puede ser sencillo si sigues algunas pautas básicas:
+
+- Riego: Riega tus plantas regularmente, pero evita el exceso de agua. Asegúrate de que el agua drene bien para prevenir la pudrición de raíces.
+  
+- Luz: Proporciona suficiente luz solar según las necesidades de cada planta. Algunas plantas prefieren luz indirecta, mientras que otras necesitan luz directa.
+  
+- Suelo: Utiliza un sustrato adecuado para cada tipo de planta. Añade compost o fertilizantes para mejorar la calidad del suelo.
+  
+- Poda: Poda las partes muertas o enfermas de la planta para fomentar un crecimiento saludable. La poda regular ayuda a mantener la forma y tamaño deseado de la planta.
+  
+- Humedad: Algunas plantas requieren alta humedad; puedes usar un humidificador o colocar un plato con agua cerca de la planta.
+  
+- Plagas: Inspecciona regularmente tus plantas para detectar plagas. Utiliza pesticidas naturales o soluciones caseras para mantener las plagas bajo control.
+
+Siguiendo estos consejos, tus plantas estarán saludables y vibrantes.
+        """,
+        unsafe_allow_html=True
+    )
+
+
+
+# Agregar botón para cambiar a la vista de los consejos de cuidado
+if st.button("Ver Consejos de Cuidado"):
+    st.sidebar.write("Consejos de Cuidado")
+    vista_consejos()
